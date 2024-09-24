6b2ce0dcc4d141765c7ef0aa58186aeed5ee4cd6
Afaq-alam <afaqalam323@gmail.com>
2024-09-24 13:20:28 +0530

    Recursion usingstatic and non static variable program inside this folder.

---
+++ Recursion/static&non_static_var_in_recursion/static_var_in_recursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+// int x = 5;  # Global variable
+
+int fun(int n)
+{
+    static int x = 0;
+    if (n > 0)
+    {
+        x++;
+        return fun(n-1) + x;
+    }
+    return 0;
+}
+
+
+int main()
+{
+    // static int x = 0;
+    int a = 5;
+    cout << fun(a);
+    return 0;
+}
